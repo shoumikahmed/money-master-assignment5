ac555d272eb1bd959f0e167ce7640f4f9b0b9f72
shoumikahmed <shoumikahmed103@gmail.com>
2022-02-17 00:11:00 +0600

    alert

--- js/script.js
+++ js/script.js
@@ -7,15 +7,26 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const clothesPrice = parseFloat(clothesCost.value)
 
     const totalExpense = document.getElementById('total-expense')
+
+    const incomeMoney = document.getElementById('income')
+    const income = parseFloat(incomeMoney.value)
     if (isNaN(foodPrice) || foodPrice < 0) {
+        return alert('please input valid amount of money in number format')
+    }
+    else if (isNaN(rentPrice) || rentPrice < 0) {
+        return alert('please input valid amount of money in number format')
+    }
+    else if (isNaN(clothesPrice) || clothesPrice < 0) {
+        return alert('please input valid amount of money in number format')
+    }
+    else if (isNaN(income) || income < 0) {
         return alert('please input valid amount of money in number format')
     }
     else {
         const newExpenseTotal = foodPrice + rentPrice + clothesPrice
         totalExpense.innerText = newExpenseTotal
         // get total balance
-        const incomeMoney = document.getElementById('income')
-        const income = parseFloat(incomeMoney.value)
+
         const totalBalance = document.getElementById('balance')
         totalBalance.innerText = income - newExpenseTotal
     }
@@ -31,7 +42,11 @@ document.getElementById('save-btn').addEventListener('click', function () {
     savingAmount.innerText = (income * saveMoney) / 100
 
     const totalBalance = document.getElementById('balance')
-
+    // get remainig balance
     const remainingBalance = document.getElementById('remaining-balance')
     remainingBalance.innerText = totalBalance.innerText - savingAmount.innerText
 })
+
+function onClick(buttonId) {
+
+}
