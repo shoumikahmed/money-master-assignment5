687a3aa97bd8ddb7738332f78626ea9ea7780b80
shoumikahmed <shoumikahmed103@gmail.com>
2022-02-16 23:32:53 +0600

    ass5

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const foodCost = document.getElementById('food-cost')
+    const foodPrice = parseFloat(foodCost.value)
+    const rentCost = document.getElementById('rent-cost')
+    const rentPrice = parseFloat(rentCost.value)
+    const clothesCost = document.getElementById('clothes-cost')
+    const clothesPrice = parseFloat(clothesCost.value)
+
+    const totalExpense = document.getElementById('total-expense')
+    if (isNaN(foodPrice) || foodPrice < 0) {
+        return alert('please input valid amount of money in number format')
+    }
+    else {
+        const newExpenseTotal = foodPrice + rentPrice + clothesPrice
+        totalExpense.innerText = newExpenseTotal
+        // get total balance
+        const incomeMoney = document.getElementById('income')
+        const income = parseFloat(incomeMoney.value)
+        const totalBalance = document.getElementById('balance')
+        totalBalance.innerText = income - newExpenseTotal
+    }
+})
+
+document.getElementById('save-btn').addEventListener('click', function () {
+    const saveInput = document.getElementById('save-input')
+})
